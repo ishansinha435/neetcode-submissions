c511ba44471c4b18eb901b828bae8836b558b538
ishansinha435 <ishan_sinha@live.com>
2026-09-09 18:38:58 -0400

    Add: two-integer-sum - submission-30

---
+++ Data Structures & Algorithms/two-integer-sum/submission-30.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hmap;
+        for (int i = 0; i < nums.size(); ++i) {
+            int diff = target - nums[i];
+            if (hmap.contains(diff)) {
+                return {hmap[diff], i};
+            }
+            hmap[nums[i]] = i;
+        }
+    }
+};
