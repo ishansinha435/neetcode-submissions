d5381f4bbf779b06e66cca93af052ec912fb72a0
ishansinha435 <ishan_sinha@live.com>
2026-09-11 21:22:17 -0400

    Add: validate-parentheses - submission-36

---
+++ Data Structures & Algorithms/validate-parentheses/submission-36.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk; 
+        unordered_map<char, char> hmap = {
+            {'(', ')'},
+            {'[', ']'},
+            {'{', '}'}
+        };
+        for (char c : s) {
+            if (hmap.contains(c)) {
+                stk.push(c);
+            }
+            else {
+                if (stk.empty() || c != hmap[stk.top()]) {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        return stk.empty();
+    }
+};
