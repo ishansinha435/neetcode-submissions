0e3a2be8790bb05942b933008e68978a2ff2255e
ishansinha435 <ishan_sinha@live.com>
2026-09-14 23:05:48 -0400

    Add: buy-and-sell-crypto - submission-14

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-14.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int res = 0;
+        for (int r = 0; r < prices.size(); ++r) {
+            if (prices[r] < prices[l]) {
+                l = r;
+            }
+            res = max(res, prices[r] - prices[l]);
+        }
+        return res;
+    }
+};
