c0756222c4e13504cae1febcc4be9ad7b5bbaac4
ishansinha435 <ishan_sinha@live.com>
2026-09-22 05:37:20 -0400

    Add: course-schedule - submission-19

---
+++ Data Structures & Algorithms/course-schedule/submission-19.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        hmap = defaultdict(list)
+        for crs, pre in prerequisites:
+            hmap[crs].append(pre)
+        visited = set()
+        memo = {}
+        
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            if i in visited:
+                return False
+            visited.add(i)
+            res = True
+            for crs in hmap[i]:
+                if not dfs(crs):
+                    res = False
+            visited.remove(i)
+            memo[i] = res
+            return res
+        
+        return all(dfs(i) for i in range(numCourses))
